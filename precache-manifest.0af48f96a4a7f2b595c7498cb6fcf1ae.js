209b8557d9dfab8bbd04dfa462321eccec7bb5cf
Dong Wook Jung <jungdw0624@gmail.com>
2019-07-27 00:58:37 +0900

    Updates

---
+++ precache-manifest.0af48f96a4a7f2b595c7498cb6fcf1ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af696084e33b4d1cc2ee119ef877999e",
+    "url": "/bibla_krv/index.html"
+  },
+  {
+    "revision": "02bd7a1fb6a7109ec913",
+    "url": "/bibla_krv/static/css/main.e4875333.chunk.css"
+  },
+  {
+    "revision": "9a2d7fa05ee6ba092c11",
+    "url": "/bibla_krv/static/js/2.40168f32.chunk.js"
+  },
+  {
+    "revision": "02bd7a1fb6a7109ec913",
+    "url": "/bibla_krv/static/js/main.a343702f.chunk.js"
+  },
+  {
+    "revision": "28a977f24a34d23be96e",
+    "url": "/bibla_krv/static/js/runtime~main.6635a495.js"
+  }
+]);
